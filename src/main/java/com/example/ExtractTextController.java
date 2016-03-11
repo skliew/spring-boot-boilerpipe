ba1cc6e4e95474ce7f66044adfdf1bca1eb68897
skliew <skliew@gmail.com>
2016-03-11 16:26:44 +0800

    Use InputSource instead of URL directly

--- src/main/java/com/example/ExtractTextController.java
+++ src/main/java/com/example/ExtractTextController.java
@@ -5,7 +5,9 @@ import de.l3s.boilerpipe.extractors.ArticleExtractor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.xml.sax.InputSource;
 
+import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -18,12 +20,17 @@ public class ExtractTextController {
         } else {
             try {
                 URL url = new URL(urlString);
-                String text = ArticleExtractor.getInstance().getText(url);
+                InputStream inputStream = url.openStream();
+                InputSource inputSource = new InputSource(inputStream);
+                String text = ArticleExtractor.getInstance().getText(inputSource);
                 return text;
             } catch (MalformedURLException e) {
                 System.err.println(e.getMessage());
                 return "";
             } catch (BoilerpipeProcessingException e) {
+                System.err.println(e.getMessage());
+                return "";
+            } catch (java.io.IOException e) {
                 System.err.println(e.getMessage());
                 return "";
             }
